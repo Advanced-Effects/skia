5fd19109c4a1536cdaf7620d3407e012696f6fc8
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2025-09-09 18:06:17 +0200

    Update CI

--- build.sh
+++ build.sh
@@ -2,15 +2,13 @@
 set -e -x
 
 APT=${APT:-0}
+OSX=${OSX:-0}
 GN="gn-src/out/gn"
 
 if [ "${APT}" = 1 ]; then
     sudo apt update -y
     sudo apt install -y libgl1-mesa-dev libglx-dev libglvnd-core-dev curl git clang build-essential python3 ninja-build libfontconfig1-dev libfreetype-dev libexpat1-dev libjpeg-turbo8-dev libpng-dev libwebp-dev zlib1g-dev libicu-dev libharfbuzz-dev
 fi
-
-rm -rf out/build-bundle || true
-rm -rf out/build-system || true
 
 git submodule update --init
 python3 tools/git-sync-deps
@@ -22,8 +20,12 @@ ninja -C out
 
 ${GN} --version
 
-${GN} gen out/build-bundle --args='is_official_build=true is_debug=false cc="clang" cxx="clang++" extra_cflags=["-Wno-error"] target_os="linux" target_cpu="x64" skia_use_system_expat=false skia_use_system_freetype2=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false skia_use_system_icu=false skia_use_system_harfbuzz=false skia_use_dng_sdk=false'
-${GN} gen out/build-system --args='is_official_build=true is_debug=false cc="clang" cxx="clang++" extra_cflags=["-Wno-error"] target_os="linux" target_cpu="x64" skia_use_system_expat=true skia_use_system_freetype2=true skia_use_system_libjpeg_turbo=true skia_use_system_libpng=true skia_use_system_libwebp=true skia_use_system_zlib=true skia_use_system_icu=true skia_use_system_harfbuzz=true skia_use_dng_sdk=false'
-
+rm -rf out/build-bundle || true
+${GN} gen out/build-bundle --args='is_official_build=true is_debug=false cc="clang" cxx="clang++" extra_cflags=["-Wno-error"] skia_use_system_expat=false skia_use_system_freetype2=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false skia_use_system_icu=false skia_use_system_harfbuzz=false skia_use_dng_sdk=false'
 ninja -C out/build-bundle skia
-ninja -C out/build-system skia
+
+if [ "${OSX}" = 0 ]; then
+    rm -rf out/build-system || true
+    ${GN} gen out/build-system --args='is_official_build=true is_debug=false cc="clang" cxx="clang++" extra_cflags=["-Wno-error"] skia_use_system_expat=true skia_use_system_freetype2=true skia_use_system_libjpeg_turbo=true skia_use_system_libpng=true skia_use_system_libwebp=true skia_use_system_zlib=true skia_use_system_icu=true skia_use_system_harfbuzz=true skia_use_dng_sdk=false'
+    ninja -C out/build-system skia
+fi
